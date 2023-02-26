8eee20cb67e0bf3895f3b1ceea45e6cd4bcc9e02
Mitch Lui <mitch@mitchlui.dev>
2023-02-26 23:13:24 +0000

    moved to Dmeo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styles from './App.module.scss';
 import Button, { AnchorButton } from './components/Button/Button';
 import Card from './components/Card/Card';
@@ -6,6 +6,7 @@ import Grid from './components/Grid/Grid';
 import PostcodeChecker from './components/PostcodeChecker/PostcodeChecker';
 import { PulsaingCircle } from './components/PulsatingCircle/PulsatingCircle';
 import { stories, Story } from './data/stories';
+import { Demo } from './components/Demo/Demo';
 
 function About({onClose = () => {}}) {
   return (
@@ -66,63 +67,73 @@ const Header = ({ text }: { text: string }) =>
 </span>
 
 function App() {
-  const [showAbout, setShowAbout] = useState(false);
+  const [showAbout, setShowAbout] = useState(false);;
+
   return (
-    <div className={styles.App} id="home">
-      {
-        showAbout && <About onClose={()=>{setShowAbout(!showAbout)}}/>
-      }
-    <NavBar onAboutClick={()=>{setShowAbout(!showAbout)}}/> 
-      <span style={{'fontSize': '25px'}}>Hello. <span style={{'color': '#0073b1'}}>Sign in</span><span style={{'fontWeight': '400'}}> for the best experience. New to Asda? </span><span style={{'color': '#0073b1'}}>Register</span></span>
-      <div style={{'marginTop': '10px','display': 'flex', 'backgroundColor': '#d5e7c7', 'justifyContent': 'space-between'}}>
-        <div style={{'margin': '15px', 'textAlign': 'left'}}>
-          <h3>We protect people, dogs and your fish and chips from the threat of seagulls,<br/>
-          and we do it with the best technology available to create our utopia.<br/>
-          <span style={{'fontWeight': '800'}}>We are <span style={{'color': '#538316'}}>ASDA</span>.</span></h3>
-          <div style={{'marginTop': '10px'}}>
-            <Button>How it works</Button>
-            <Button>See it in action</Button>
-          </div>
-        </div>
-        <div style={{'margin': 'auto 30px auto 0px', 'textAlign': 'right', 'display': 'inline-block',}}>
-          <PostcodeChecker/>
-        </div>
-      </div>
-      <div id="technology" style={{'marginTop': '10px','display': 'flex', 'backgroundColor': '#ebebeb', 'justifyContent': 'space-between'}}>        
-        <div style={{'marginLeft': '16px', 'width': '50%', zIndex: '1', 'textAlign': 'left'}}>
-          <h1 style={{'color': '#538316', 'paddingTop': '15px'}}>Technology</h1>
-          <span style={{'fontSize': '20px'}}>Using our proprietary SonicBarrier technology, we can prevent seagulls from entering a protected area
-          by emitting a high frequency sound that is inaudible to humans but deters seagulls.</span>
-          <p><Button>See it in action</Button></p>
-        </div>
-        <PulsaingCircle/>
-      </div>
-      <div id="stories">
-        <Header text={"Stories"}/>
-        <Grid>
-          {
-            stories.map((story: Story) => {
-              return (
-                <Card 
-                  key={story.id} 
-                  title={story.title} 
-                  description={story.description} 
-                  detail={story.detail} 
-                  image={story.image}
-                  imageAlt={story.imageAlt}
-                  storyURL={story.storyURL}
-                />
-              );
-            })
-          }
-        </Grid>
-      </div>
-      <div style={{'textAlign': 'center', 'padding': '16px'}}>
-        <Button>See the demo</Button>
-        <Button>Buy an ASDA</Button>
-      </div>
-      <div style={{'textAlign': 'center', 'fontWeight': '600'}}>N.B. This is a student project and is not in any way affiliated with ASDA the supermarket chain.</div>
-    </div>
+    <Demo/>
+    // <div className={styles.App} id="home">
+    //   {
+    //     showAbout && <About onClose={()=>{setShowAbout(!showAbout)}}/>
+    //   }
+    // <NavBar onAboutClick={()=>{setShowAbout(!showAbout)}}/> 
+    //   <span style={{'fontSize': '25px'}}>Hello. <span style={{'color': '#0073b1'}}>Sign in</span><span style={{'fontWeight': '400'}}> for the best experience. New to Asda? </span><span style={{'color': '#0073b1'}}>Register</span></span>
+    //   <div style={{'marginTop': '10px','display': 'flex', 'backgroundColor': '#d5e7c7', 'justifyContent': 'space-between'}}>
+    //     <div style={{'margin': '15px', 'textAlign': 'left'}}>
+    //       <h3>We protect people, dogs and your fish and chips from the threat of seagulls,<br/>
+    //       and we do it with the best technology available to create our utopia.<br/>
+    //       <span style={{'fontWeight': '800'}}>We are <span style={{'color': '#538316'}}>ASDA</span>.</span></h3>
+    //       <div style={{'marginTop': '10px'}}>
+    //         <Button>How it works</Button>
+    //         <Button>See it in action</Button>
+    //       </div>
+    //       </div>
+    //     <div style={{'margin': 'auto 30px auto 0px', 'textAlign': 'right', 'display': 'inline-block',}}>
+    //       <PostcodeChecker/>
+    //     </div>
+    //   </div>
+    //   <div>
+    //   <div id="loading"><p>Loading...</p></div>
+    //         <div id="liveView" className="videoView">
+    //           <video id="video" loop autoPlay>
+    //             <source src="/videos/7.mp4" type="video/mp4" />
+    //           </video>
+    //         </div>
+    //   </div>
+    //   <div id="technology" style={{'marginTop': '10px','display': 'flex', 'backgroundColor': '#ebebeb', 'justifyContent': 'space-between'}}>        
+    //     <div style={{'marginLeft': '16px', 'width': '50%', zIndex: '1', 'textAlign': 'left'}}>
+    //       <h1 style={{'color': '#538316', 'paddingTop': '15px'}}>Technology</h1>
+    //       <span style={{'fontSize': '20px'}}>Using our proprietary SonicBarrier technology, we can prevent seagulls from entering a protected area
+    //       by emitting a high frequency sound that is inaudible to humans but deters seagulls.</span>
+    //       <p><Button>See it in action</Button></p>
+    //     </div>
+    //     <PulsaingCircle/>
+    //   </div>
+    //   <div id="stories">
+    //     <Header text={"Stories"}/>
+    //     <Grid>
+    //       {
+    //         stories.map((story: Story) => {
+    //           return (
+    //             <Card 
+    //               key={story.id} 
+    //               title={story.title} 
+    //               description={story.description} 
+    //               detail={story.detail} 
+    //               image={story.image}
+    //               imageAlt={story.imageAlt}
+    //               storyURL={story.storyURL}
+    //             />
+    //           );
+    //         })
+    //       }
+    //     </Grid>
+    //   </div>
+    //   <div style={{'textAlign': 'center', 'padding': '16px'}}>
+    //     <Button>See the demo</Button>
+    //     <Button>Buy an ASDA</Button>
+    //   </div>
+    //   <div style={{'textAlign': 'center', 'fontWeight': '600'}}>N.B. This is a student project and is not in any way affiliated with ASDA the supermarket chain.</div>
+    // </div>
   );
 }
 
---
+++ frontend/src/components/Demo/Demo.jsx
@@ -0,0 +1,208 @@
+import { useEffect } from 'react';
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+
+async function test() {
+  var model = undefined;
+  var video = document.getElementById('video');
+  const liveView = document.getElementById('liveView');
+  var bboxes = [];
+
+  // We will create a tempory canvas to render to store frames from 
+  // the web cam stream for classification.
+  var videoRenderCanvas = document.createElement('canvas');
+  var videoRenderCanvasCtx = videoRenderCanvas.getContext('2d');
+
+  // Lets create a canvas to render our findings to the DOM.
+  var displayedCanvas = document.createElement('canvas');
+  displayedCanvas.setAttribute('class', 'overlay');
+  liveView.appendChild(displayedCanvas);
+
+  var birdClicked = false;
+
+  function handleClick(event) {
+    var bboxesCopy = [...bboxes];
+    for (let i = 0; i < bboxesCopy.length; i++) {
+      var bbox = bboxesCopy[i];
+      var x = bbox[0];
+      var y = bbox[1];
+      var w = bbox[2];
+      var h = bbox[3];
+      if (event.offsetX > x && event.offsetX < x + w && event.offsetY > y && event.offsetY < y + h) {
+        birdClicked = true;
+      }
+    }
+  }
+
+  /********************************************************************
+  // Continuously grab image from displayed stream and classify it.
+  ********************************************************************/
+  var previousSegmentationComplete = true;
+
+  function maskObject(bbox) {
+    var canvas = displayedCanvas;
+
+    var ctx = canvas.getContext('2d');
+    // Get data from our overlay canvas which is attempting to estimate background.
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    
+    // Get data from the live displayed view which has all data.
+    var liveData = videoRenderCanvasCtx.getImageData(0, 0, canvas.width, canvas.height);
+    var dataL = liveData.data;
+
+    if (birdClicked) {
+      // Update video
+      var scale = 1.5;
+      var minX = bbox[0];
+      var minY = bbox[1];
+      var width = bbox[2];
+      var height = bbox[3];
+      //  Define scaled dimensions.
+      var newWidth = width * scale;
+      var newHeight = height * scale;
+      // Calculate the offset to place new bounding box so scaled from center of current bounding box.
+      var offsetX = (newWidth - width) / 2;
+      var offsetY = (newHeight - height) / 2;
+      var newXMin = minX - offsetX;
+      var newYMin = minY - offsetY;
+      
+      // Now loop through update backgound understanding with new data
+      // if not inside a bounding box.
+      for (let x = 0; x < canvas.width; x++) {
+        for (let y = 0; y < canvas.height; y++) {
+          // If outside bounding box and we found a body, update background.
+          if ((x < newXMin || x > newXMin + newWidth) || ( y < newYMin || y > newYMin + newHeight)) {
+            // Convert xy co-ords to array offset.
+            let n = y * canvas.width + x;
+    
+            data[n * 4] = dataL[n * 4];
+            data[n * 4 + 1] = dataL[n * 4 + 1];
+            data[n * 4 + 2] = dataL[n * 4 + 2];
+            data[n * 4 + 3] = 255;            
+    
+          }
+        }
+      }
+    }
+
+    else {
+      for (let x = 0; x < canvas.width; x++) {
+        for (let y = 0; y < canvas.height; y++) {
+          // Convert xy co-ords to array offset.
+          let n = y * canvas.width + x;
+
+          data[n * 4] = dataL[n * 4];
+          data[n * 4 + 1] = dataL[n * 4 + 1];
+          data[n * 4 + 2] = dataL[n * 4 + 2];
+          data[n * 4 + 3] = 255;            
+        }
+      }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+  }
+
+  var prev_box = undefined;
+
+  function predict() {
+    if (previousSegmentationComplete) {
+      model.detect(video).then(predictions => {
+        // delete all previous predictions
+        // for (let i = 0; i < children.length; i++) {
+        //   liveView.removeChild(children[i]);
+        // }
+        // children.splice(0);
+
+        bboxes = []; // empty bboxes
+
+        // Add bboxes with score > 0.5
+        predictions.sort((a, b) => b.score - a.score);
+        for (let n = 0; n < predictions.length; n++) {
+          if (predictions[n].score > 0.2) {
+            bboxes.push(predictions[n].bbox);
+            const p = document.createElement('p');
+            p.innerText = predictions[n].class  + ' - with ' 
+                + Math.round(parseFloat(predictions[n].score) * 100) 
+                + '% confidence.';
+            p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+                + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+                + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+            const highlighter = document.createElement('div');
+            highlighter.setAttribute('class', 'highlighter');
+            highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+                + predictions[n].bbox[1] + 'px; width: ' 
+                + predictions[n].bbox[2] + 'px; height: '
+                + predictions[n].bbox[3] + 'px;';
+
+            // liveView.appendChild(highlighter);
+            // liveView.appendChild(p);
+            // children.push(highlighter);
+            // children.push(p);
+          }
+        }
+
+        videoRenderCanvasCtx.drawImage(video, 0, 0);
+        previousSegmentationComplete = false;
+        if (bboxes.length > 0) {
+          prev_box = bboxes[0];
+        }
+        maskObject(prev_box);
+        previousSegmentationComplete = true;
+
+        window.requestAnimationFrame(predict);
+      });
+    }
+  }
+
+  // Load the model;
+  cocoSsd.load().then(loadedModel => {
+    // detect objects in the image.
+    model = loadedModel;
+    console.log("Loaded");
+    var loading = document.getElementById('loading');
+    loading.style="display: none;";
+
+    // Video stuff
+    liveView.addEventListener('click', handleClick);
+
+    // Set up canvases
+    displayedCanvas.width = video.videoWidth;
+    displayedCanvas.height = video.videoHeight;
+    videoRenderCanvas.width = video.videoWidth;
+    videoRenderCanvas.height = video.videoHeight;
+    let displayedCanvasCtx = displayedCanvas.getContext('2d');
+    displayedCanvasCtx.drawImage(video, 0, 0);
+
+    // Start the video
+    document.addEventListener('mousemove', function(e) {
+      let circle = document.getElementById("crosshair");
+      circle.style.left = e.pageX + 'px';
+      circle.style.top = e.pageY + 'px';
+    });
+    
+    video.play();
+    predict();
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+export const Demo = () => {
+
+  useEffect(() => {
+    test();
+  }, []);
+
+  return (
+    <>
+    <div id="loading"><p>Loading...</p></div>
+    <div id="liveView" className="videoView">
+      <video id="video" loop style={{'display': 'none'}}>
+        <source src="videos/7.mp4" type="video/mp4"/>
+      </video>
+    </div>
+    <img src="/cross.png" alt="img" id="crosshair"/>
+    </>
+  )
+}
