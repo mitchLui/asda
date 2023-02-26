193825708b3b81b06398a1c2d6333434a2aa05bd
JoanneLin168 <joannelin168@outlook.com>
2023-02-26 15:51:31 +0000

    click birb

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const liveView = document.getElementById('liveView');
 // Keep a reference of all the child elements we create
 // so we can remove them easily on each render.
 var children = [];
+var bboxes = [];
+
+function handleClick(event) {
+  var bboxesCopy = [...bboxes];
+  for (let i = 0; i < bboxesCopy.length; i++) {
+    var bbox = bboxesCopy[i];
+    var x = bbox[0];
+    var y = bbox[1];
+    var w = bbox[2];
+    var h = bbox[3];
+    if (event.offsetX > x && event.offsetX < x + w && event.offsetY > y && event.offsetY < y + h) {
+      // var p = child.nextSibling;
+      // var text = p.innerText;
+      console.log("Bird clicked: " + i);
+    }
+  }
+}
 
 function predict() {
   model.detect(video).then(predictions => {
@@ -13,6 +30,7 @@ function predict() {
       liveView.removeChild(children[i]);
     }
     children.splice(0);
+    bboxes = []; // empty bboxes
 
     // Now lets loop through predictions and draw them to the live view if
     // they have a high confidence score.
@@ -34,10 +52,11 @@ function predict() {
             + predictions[n].bbox[2] + 'px; height: '
             + predictions[n].bbox[3] + 'px;';
 
-        liveView.appendChild(highlighter);
-        liveView.appendChild(p);
-        children.push(highlighter);
-        children.push(p);
+        // liveView.appendChild(highlighter);
+        // liveView.appendChild(p);
+        // children.push(highlighter);
+        // children.push(p);
+        bboxes.push(predictions[n].bbox);
       }
     }
     window.requestAnimationFrame(predict);
@@ -49,7 +68,12 @@ cocoSsd.load().then(loadedModel => {
   // detect objects in the image.
   model = loadedModel;
   console.log("Loaded");
+  var loading = document.getElementById('loading');
+  loading.style="display: none;";
+
+  // Video stuff
   video.style="display: block;";
+  liveView.addEventListener('click', handleClick);
   video.play();
   predict();
 });
